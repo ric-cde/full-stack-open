cd315171bf0fa9d96ef2363f291eb22d51ca68cd
richardherlihy@gmail.com <39746198+ric-cde@users.noreply.github.com>
2023-06-13 18:03:01 +0100

    aws config changes

--- part3/a_practice/index.js
+++ part3/a_practice/index.js
@@ -97,7 +97,7 @@ app.post('/api/notes', (req, res) => {
   }
 )
 
-const PORT = process.env.PORT || 3001
+const PORT = 3001 // process.env.PORT || 3001
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
 })
