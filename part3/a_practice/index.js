76d23ba9b5416b38f91e84a89591ed471ff5484d
richardherlihy@gmail.com <39746198+ric-cde@users.noreply.github.com>
2023-06-26 17:06:41 +0100

    uibuild

--- part3/a_practice/index.js
+++ part3/a_practice/index.js
@@ -1,49 +1,58 @@
 const express = require('express')
 const app = express()
-app.use(express.static('build'))
-
 const cors = require('cors')
-app.use(cors())
+require('dotenv').config()
 
-app.use(express.json())
 const requestLogger = (req, res, next) => {
   console.log('Method:', req.method)
   console.log('Path: ', req.path)
   console.log('Body: ', req.body)
   console.log('---');
   next()
+  }
+
+const unknownEndpoint = (req, res) => {
+  response.status(404).send({error: 'unknown endpoint'})
 }
+  
+app.use(cors())
+app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('build'))
+
+const Note = require('./models/note')
 
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2022-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2022-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2022-05-30T19:20:14.298Z",
-      important: true
-    }
-  ]
+// let notes = [
+//     {
+//       id: 1,
+//       content: "HTML is easy",
+//       date: "2022-05-30T17:30:31.098Z",
+//       important: true
+//     },
+//     {
+//       id: 2,
+//       content: "Browser can execute only Javascript",
+//       date: "2022-05-30T18:39:34.091Z",
+//       important: false
+//     },
+//     {
+//       id: 3,
+//       content: "GET and POST are the most important methods of HTTP protocol",
+//       date: "2022-05-30T19:20:14.298Z",
+//       important: true
+//     }
+//   ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World</h1>')
-})
+// app.get('/', (request, response) => {
+//     response.send('<h1>Hello World</h1>')
+// })
 
 app.get('/api/notes', (request, response) => {
     console.log('returning all notes... ')
-    response.json(notes)
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (req, res) => {
@@ -99,13 +108,9 @@ app.post('/api/notes', (req, res) => {
   }
 )
 
-const PORT = process.env.PORT || 3001
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
 })
-
-const unknownEndpoint = (req, res) => {
-  response.status(404).send({error: 'unknown endpoint'})
-}
-
-app.use(unknownEndpoint)
