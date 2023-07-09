9c8516f874a10cee41ae306f392983aae05f241b
Richard Herlihy <richardherlihy@gmail.com>
2023-07-09 15:26:11 +0100

    uibuild

--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -22,7 +22,13 @@ const personSchema = new mongoose.Schema({
 	},
 	number: {
 		type: String,
-		minLength: [5, "Must be at least 5 digits"],
+		minLength: [9, "Must be at least 8 digits and include a dash"],
+		validate: {
+			validator: function (v) {
+				return /\d{2,3}-\d+/.test(v);
+			},
+			message: (props) => `${props.value} is not a valid phone number!`,
+		},
 		required: true,
 	},
 });
