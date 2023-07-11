efdf4def4f53ae65428c5a5df2f4e6a15333241f
Richard Herlihy <richardherlihy@gmail.com>
2023-07-11 18:37:38 +0100

    part 4 started, fixed gitignore, removed node_modules

---
+++ part4/blog_list/models/blog.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose")
+
+const blogSchema = new mongoose.Schema({
+	title: {
+		type: String,
+		minLength: 5,
+		required: true,
+	},
+	author: {
+		type: String,
+		minLength: 3,
+		required: true,
+	},
+	url: {
+		type: String,
+		minLength: 7,
+		required: true,
+	},
+	likes: {
+		type: Number,
+		required: true,
+	}
+})
+
+blogSchema.set("toJSON", {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	},
+})
+
+module.exports = mongoose.model("Blog", blogSchema)
+
